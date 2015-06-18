79975d9fdc490652c39cfbf1c6728b988b5d8ef9
Daniel Hoffmann <oralordos@gmail.com>
2015-06-18 12:00:46 -0700

    Added adding data and removing data.

--- SummerWebBootcamp/Week4/routing/artists.js
+++ SummerWebBootcamp/Week4/routing/artists.js
@@ -4,6 +4,15 @@ artistApp.controller('listController', ['$scope', '$firebaseArray', function($sc
     var ref = new Firebase('https://angular-bootcamp.firebaseio.com/');
 
     $scope.people = $firebaseArray(ref.child('people'));
+    $scope.delete = function(itemId) {
+        ref.child('people/' + itemId).remove();
+    }
+    $scope.add = function() {
+        if($scope.newItem.name && $scope.newItem.shortname && $scope.newItem.reknown && $scope.newItem.bio) {
+            ref.child('people').push($scope.newItem);
+            $scope.newItem = {};
+        }
+    }
 }]);
 
 artistApp.controller('detailController', ['$scope', '$routeParams', '$firebaseObject', function($scope, $routeParams, $firebaseObject) {
