93cb1988c08585dad5c0c2866c3c5ae8507d0bac
Daniel Hoffmann <oralordos@gmail.com>
2015-06-19 10:17:43 -0700

    Added image upload

--- SummerWebBootcamp/Week4/routing/artists.js
+++ SummerWebBootcamp/Week4/routing/artists.js
@@ -1,16 +1,21 @@
 var artistApp = angular.module('artists', ['firebase', 'ngAnimate']);
 
 artistApp.controller('listController', ['$scope', '$firebaseArray', function($scope, $firebaseArray) {
-    var ref = new Firebase('https://angular-bootcamp.firebaseio.com/');
+    var ref = new Firebase('https://angular-bootcamp.firebaseio.com/people');
 
-    $scope.people = $firebaseArray(ref.child('people'));
+    $scope.people = $firebaseArray(ref);
     $scope.delete = function(itemId) {
-        ref.child('people/' + itemId).remove();
-    }
+        ref.child(itemId).remove();
+    };
     $scope.add = function() {
-        if($scope.newItem.name && $scope.newItem.shortname && $scope.newItem.reknown && $scope.newItem.bio) {
-            ref.child('people').push($scope.newItem);
-            $scope.newItem = {};
+        if($scope.newItem.name && $scope.newItem.reknown && $scope.newItem.bio) {
+            var reader = new FileReader();
+            reader.addEventListener('load', function(e) {
+                $scope.newItem.image = e.target.result;
+                ref.push($scope.newItem);
+                $scope.newItem = {};
+            });
+            reader.readAsDataURL(document.forms.addPerson.picture.files[0]);
         }
     }
 }]);
