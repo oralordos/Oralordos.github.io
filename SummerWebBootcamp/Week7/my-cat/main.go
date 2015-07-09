8f1f27b777b18005a51ffe15e4073f9aaecab8f9
Daniel Hoffmann <oralordos@gmail.com>
2015-07-09 14:31:23 -0700

    Added word count project and edited my-cat project

--- SummerWebBootcamp/Week7/my-cat/main.go
+++ SummerWebBootcamp/Week7/my-cat/main.go
@@ -1,40 +1,26 @@
 package main
 
 import (
-	"fmt"
-	"io/ioutil"
+	"io"
 	"log"
 	"os"
 )
 
 func main() {
-	if len(os.Args) < 3 {
+	if len(os.Args) < 2 {
 		log.Fatalln("Not enough command line arguments")
 	}
 
-	readFile, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatalln(err)
-	}
-	defer readFile.Close()
-
-	bytes, err := ioutil.ReadAll(readFile)
-	if err != nil {
-		log.Fatalln(err)
-	}
-
-	if len(os.Args) >= 2 {
-		writeFile, err := os.Create(os.Args[2])
+	readers := make([]io.Reader, len(os.Args)-1)
+	for i, v := range os.Args[1:] {
+		readFile, err := os.Open(v)
 		if err != nil {
 			log.Fatalln(err)
 		}
-		defer writeFile.Close()
-		_, err = writeFile.Write(bytes)
-		if err != nil {
-			log.Fatalln(err)
-		}
-	} else {
-		str := string(bytes)
-		fmt.Println(str)
+		defer readFile.Close()
+		readers[i] = readFile
 	}
+	rdr := io.MultiReader(readers...)
+
+	io.Copy(os.Stdout, rdr)
 }
