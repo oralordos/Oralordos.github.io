0f6d541b8855980b1b3e947c5b81db0adff400d5
Daniel Hoffmann <oralordos@gmail.com>
2015-07-10 11:32:57 -0700

    Made md5 walk a directory

--- SummerWebBootcamp/Week7/my-md5/main.go
+++ SummerWebBootcamp/Week7/my-md5/main.go
@@ -6,6 +6,7 @@ import (
 	"io"
 	"log"
 	"os"
+	"path/filepath"
 )
 
 func main() {
@@ -13,13 +14,20 @@ func main() {
 		log.Fatalln("Please pass in a filename to calculate the md5 of")
 	}
 
-	f, err := os.Open(os.Args[1])
+	err := filepath.Walk(os.Args[1], func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			f, err := os.Open(path)
+			if err != nil {
+				return err
+			}
+			defer f.Close()
+			hash := md5.New()
+			io.Copy(hash, f)
+			fmt.Printf("%x\t%s\n", hash.Sum(nil), info.Name())
+		}
+		return nil
+	})
 	if err != nil {
 		log.Fatalln(err.Error())
 	}
-	defer f.Close()
-
-	hash := md5.New()
-	io.Copy(hash, f)
-	fmt.Printf("%x\n", hash.Sum(nil))
 }
